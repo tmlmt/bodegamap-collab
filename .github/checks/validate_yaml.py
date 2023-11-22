80c0320d3a5eeffee3cffb3564cca5c29ef20f3b
Thomas Lamant <tom@tmlmt.com>
2023-11-22 04:03:29 +0100

    chore: merge main into branch

--- .github/checks/validate_yaml.py
+++ .github/checks/validate_yaml.py
@@ -18,34 +18,44 @@ def validate_yaml_files(directory):
 
     return True
 
+def find_line_number(file_path, target_key):
+    with open(file_path, 'r') as f:
+        for line_number, line in enumerate(f, start=1):
+            if target_text + ":" in line:
+                return line_number
+    return None
+
 def validate_yaml(yaml_file):
     errors_found = False
     first_error = True
     with open(yaml_file, 'r') as f:
         try:
             data = yaml.safe_load(f)
-
-            if not isinstance(data, dict):
-                print(f"::group::{yaml_file}")
-                print(f"Invalid YAML file: {yaml_file}")
-                print("Top-level structure should be a dictionary")
-                print(f"::endgroup::")
-                return False
-
-            for key, value in data.items():
-                if not isinstance(key, str) or not isinstance(value, str):
-                    errors_found = True
-                    if first_error:
-                        print(f"::group::{yaml_file}")
-                        first_error = False
-                    print(f"::error file={yaml_file},line={f.tell() - len(key) - 2}::Incorrect syntax. Check that the value is a valid yaml string")
-
         except yaml.YAMLError as e:
             print(f"::group::{yaml_file}")
             print(f"Invalid YAML file: {yaml_file}")
             print(e)
             print(f"::endgroup::")
             return False
+
+    if not isinstance(data, dict):
+        print(f"::group::{yaml_file}")
+        print(f"Invalid YAML file: {yaml_file}")
+        print("Top-level structure should be a dictionary")
+        print(f"::endgroup::")
+        return False
+
+    for key, value in data.items():
+        if not isinstance(key, str) or not isinstance(value, str):
+            errors_found = True
+            if first_error:
+                print(f"::group::{yaml_file}")
+                first_error = False
+            line_number = find_line_number(yaml_file,key)
+            if not line_number:
+                print(f"::error ::Incorrect syntax at unknown line. Check that all keys and values are valid yaml strings")
+            else:
+                print(f"::error file={yaml_file},line={line_number}::Incorrect syntax in line {line_number}. Check that the value is a valid yaml string")
 
     if errors_found:
         print(f"::endgroup::")
